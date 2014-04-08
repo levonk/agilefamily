a48ac1dd6a86e9e450cf79dc898da2aa9004404b
Levon Karayan <levon@tweetup.com>
2014-04-08 06:29:37 -0700

    WIP Person

--- modules/model/src/main/java/com/levonk/model/agile/Person.java
+++ modules/model/src/main/java/com/levonk/model/agile/Person.java
@@ -3,5 +3,12 @@ import com.google.auto.value.AutoValue;
 @AutoValue
 public abstract class Person
 {
-	public abstract String name();
+	public static Person()
+	{
+	}
+
+	public abstract String getName();
+	public abstract ZonedDateTime getCreateDateTime();
+	public abstract ZonedDateTime getModifyDateTime();
+	// PersonID
 }
